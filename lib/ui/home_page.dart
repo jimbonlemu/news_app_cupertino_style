8dec26f93514a7abc60bfe476f610cee964ede0d
perompak_kode <iqbaltech29@gmail.com>
2024-01-18 09:52:39 +0700

    update : file structure changes and code improvements

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:dicoding_news_app/article_list_page.dart';
-import 'package:dicoding_news_app/settings_page.dart';
-import 'package:dicoding_news_app/styles.dart';
+import 'dart:io';
+
+import 'package:dicoding_news_app/ui/article_list_page.dart';
+import 'package:dicoding_news_app/ui/settings_page.dart';
+import 'package:dicoding_news_app/common/styles.dart';
 import 'package:dicoding_news_app/widgets/platform_widget.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -16,6 +18,21 @@ class NewsListPage extends StatefulWidget {
 }
 
 class _NewsListPageState extends State<NewsListPage> {
+  List<BottomNavigationBarItem> _bottomNavBarItems = [
+    BottomNavigationBarItem(
+      icon: Icon(Platform.isIOS ? CupertinoIcons.news : Icons.public),
+      label: 'Headline',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Platform.isIOS ? CupertinoIcons.settings : Icons.settings),
+      label: 'Settings',
+    ),
+  ];
+
+  final List<Widget> _listWidget = [
+    ArticleListPage(),
+    SettingsPage(),
+  ];
   int _bottomNavIndex = 0;
 
   @override
@@ -28,21 +45,11 @@ class _NewsListPageState extends State<NewsListPage> {
 
   Widget _buildAndroid(BuildContext context) {
     return Scaffold(
-      body:
-          _bottomNavIndex == 0 ? const ArticleListPage() : const SettingsPage(),
+      body: _listWidget[_bottomNavIndex],
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: secondaryColor,
         currentIndex: _bottomNavIndex,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.public),
-            label: 'Headline',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
+        items: _bottomNavBarItems,
         onTap: (selected) {
           setState(() {
             _bottomNavIndex = selected;
@@ -56,16 +63,7 @@ class _NewsListPageState extends State<NewsListPage> {
     return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
         activeColor: secondaryColor,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.news),
-            label: 'Headline',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.settings),
-            label: 'Settings',
-          ),
-        ],
+        items: _bottomNavBarItems,
       ),
       tabBuilder: (context, index) {
         switch (index) {
